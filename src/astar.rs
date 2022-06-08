a1b287f97b90fcbd0341cb7f6cbfdc8e584a9aed
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-06-08 23:19:50 +0200

    Add logic for next/neighbouring nodes to check

--- src/astar.rs
+++ src/astar.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::cell::RefCell;
 
 use colored::Colorize;
@@ -10,19 +11,19 @@ lazy_static! {
     /// base g cost
     static ref BASE_G_COST: i32 = 10;
     /// indicator for start position
-    static ref START_INDICATOR: usize = 8;
+    static ref START_INDICATOR: i32 = 8;
     /// indicator for end position
-    static ref END_INDICATOR: usize = 9;
+    static ref END_INDICATOR: i32 = 9;
     /// the max board size
-    static ref MAX_BOARD_SIZE: usize = 13;
+    static ref MAX_BOARD_SIZE: i32 = 13;
     /// the blocked node value
-    static ref BLOCKED_NODE: usize = 1;
+    static ref BLOCKED_NODE: i32 = 1;
     /// the free node value
-    static ref FREE_NODE: usize = 0;
+    static ref FREE_NODE: i32 = 0;
 }
 
 /// random generation of blocked nodes
-pub fn gen_blockade() -> usize {
+pub fn gen_blockade() -> i32 {
     if thread_rng().gen_ratio(1, 7) {
         // 1 in 7 chance to get a blocked node
         *BLOCKED_NODE
@@ -31,13 +32,13 @@ pub fn gen_blockade() -> usize {
     }
 }
 
-pub fn gen_range(x: usize) -> usize {
+pub fn gen_range(x: i32) -> i32 {
     thread_rng().gen_range(x..*MAX_BOARD_SIZE)
 }
 
 /// get a random position on the board
 /// used to create start and end points
-pub fn get_rand_pos(height: usize, width: usize) -> (usize, usize) {
+pub fn get_rand_pos(height: i32, width: i32) -> (i32, i32) {
     (
         thread_rng().gen_range(0..height),
         thread_rng().gen_range(0..width),
@@ -45,27 +46,22 @@ pub fn get_rand_pos(height: usize, width: usize) -> (usize, usize) {
 }
 
 /// generate a 2d board
-pub fn gen_board(
-    height: usize,
-    width: usize,
-    start: (usize, usize),
-    end: (usize, usize),
-) -> Vec<Vec<usize>> {
-    let mut result: Vec<Vec<usize>> = Vec::new();
+pub fn gen_board(height: i32, width: i32, start: (i32, i32), end: (i32, i32)) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = Vec::new();
     for _ in 0..height {
-        let mut new_vec: Vec<usize> = vec![];
+        let mut new_vec: Vec<i32> = vec![];
         for _ in 0..width {
             new_vec.push(gen_blockade());
         }
         result.push(new_vec);
     }
-    result[start.0][start.1] = *START_INDICATOR; // start indicator
-    result[end.0][end.1] = *END_INDICATOR; // end indicator
+    result[start.0 as usize][start.1 as usize] = *START_INDICATOR; // start indicator
+    result[end.0 as usize][end.1 as usize] = *END_INDICATOR; // end indicator
     result
 }
 
 /// draw the board in stdout
-pub fn draw_board(board: Vec<Vec<usize>>, selected: Vec<(usize, usize)>) {
+pub fn draw_board(board: Vec<Vec<i32>>, selected: Vec<(i32, i32)>) {
     print!("     ");
     for y in 0..board[0].len() {
         print!("{} ", y);
@@ -81,7 +77,7 @@ pub fn draw_board(board: Vec<Vec<usize>>, selected: Vec<(usize, usize)>) {
             // draw end in red
             let mut is_selected = false;
             for (x1, y1) in &selected {
-                if i == *x1 && j == *y1 {
+                if i as i32 == *x1 && j as i32 == *y1 {
                     is_selected = true;
                 }
             }
@@ -111,7 +107,7 @@ pub struct AStar {
     /// the end node
     pub end: Node,
     /// the game board
-    pub board: RefCell<Vec<Vec<usize>>>,
+    pub board: RefCell<Vec<Vec<i32>>>,
     /// the list of the solved path
     pub solved_path: RefCell<Vec<Node>>,
     /// the current neighbours
@@ -144,9 +140,9 @@ impl Default for AStar {
 #[derive(Debug, Clone)]
 pub struct Node {
     /// x,height, column
-    pub x: usize,
+    pub x: i32,
     /// y, width, row
-    pub y: usize,
+    pub y: i32,
 }
 
 // A Node represented as a x and y coordinate in the planes of the game world
@@ -220,8 +216,8 @@ impl AStar {
     /// solve the board
     pub fn solve(&mut self) {
         let board = self.board.clone().into_inner();
-        let height = board.len();
-        let width = board[0].clone().len();
+        let height: i32 = board.len() as i32;
+        let width: i32 = board[0].clone().len() as i32;
         let mut pos = get_rand_pos(height, width);
 
         // debug
@@ -244,11 +240,32 @@ impl AStar {
         );
     }
 
+    /// generate a list of relative nodes from the selected/start node to be checked next
     pub fn gen_surrounding(&mut self) {
-        for _ in -1..1 {
-            for _ in -1..1 {
-                self.neighbours_list.borrow_mut().push(Node { x: 0, y: 0 });
+        for x in -1_i32..2_i32 {
+            for y in -1_i32..2_i32 {
+                let start = self.start.borrow();
+                let board_x = self.board.borrow().len() as i32 - 1;
+                let board_y = self.board.borrow()[0].len() as i32 - 1;
+                let r_x = start.x + x; // relative position from start/current node
+                let r_y = start.y + y;
+                // cases:
+                // don't add start node
+                // don't add nodes out of bounds
+                // don't add nodes that are blocked
+                if !(r_x == start.x && r_y == start.y)
+                    && ((r_x >= 0 && r_x <= board_x) && (r_y >= 0 && r_y <= board_y))
+                {
+                    let current_node = self.board.borrow()[r_x as usize][r_y as usize];
+                    if current_node != 1 {
+                        self.neighbours_list.borrow_mut().push(Node {
+                            x: start.x + x as i32,
+                            y: start.y + y as i32,
+                        });
+                    }
+                }
             }
         }
+        println!("{:?}", self.neighbours_list.borrow());
     }
 }
